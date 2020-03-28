ae80a98f16e2f57967ba22e6ffa47cea56651a4b
Priyankadubey190 <priyankadubey0811@gmail.com>
2020-03-28 07:02:50 -0700

    Add React city project

---
+++ city-tours/src/tourData.js
@@ -0,0 +1,26 @@
+export const tourData = [
+    {
+        id:1,
+        city:"new york",
+        img:"./Images/Medical1.jpg",
+        name:"new york bridge tour",
+        info:"Lorem ipsum, dolor sit amet consectetur adipisicingelit."
+
+    },
+    {
+        id :2,
+        city:"paris",
+        img:"./Images/medical2.jpg",
+        name:"paris lights tour",
+        info:"Lorem ipsum, dolor sit amet consectetur  adipisicing elit."
+             
+    },
+    {
+        id :3,
+        city:"paris",
+        img:"./Images/Medical3.jpg",
+        name:"paris lights tour",
+        info:"Lorem ipsum, dolor sit amet consectetur  adipisicing elit."
+             
+    }
+];
